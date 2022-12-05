ee911ea8fa786bd403ff72038cc7a938ffe9dfe1
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2022-12-05 15:54:30 +0100

    update

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const func = require('./src/app')
 //this reset boolean resets the main process once an execution is run completely
-var reset = true
 
-module.exports = {
-  reset,
-}
+
 
 const express = require('express')
 
 const app = express()
 
 app.use(express.json())
-
+var reset = true
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server Started `)
+
+  console.log(`Server Started at ${process.env.PORT} `)
   //Reset key is passed to main
   func.main(reset)
 })
+
+module.exports = {
+  reset,
+}
